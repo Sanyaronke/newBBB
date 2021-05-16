a34db4f479fb5d47def85c55aa8276f1b2854287
Anyaronke Samuel <nous.devellonpons@gmail.com>
2021-05-16 19:52:45 +0200

    modication the route. Add route name, post route

--- routers/Route.php
+++ routers/Route.php
@@ -2,35 +2,71 @@
 
 namespace Route;
 
-class Route {
-    public $path;
-    public $action;
-    public $matches;
+class Route
+{
+    // pour stocker nos route
+    private static $request;
 
-    public function __construct( $path, $action) {
-        $this->path = trim($path, '/');
-        $this->action = $action;
+
+    // public $matches;
+    
+    public static function get(string $paht, string $action)
+    {
+        // on stock l'intance qui va permetre de faire les match dans routes
+        $routes = new Request($paht, $action);
+        // stocage de route dans request
+        self::$request['GET'][] = $routes;
+        return $routes;
     }
 
-    public function matches(string $url)
+    public static function post(string $paht, string $action)
     {
-        $path = preg_replace('#:([\w]+)#', '([^/]+)', $this->path); 
-        $pathToMatch = "#^$path$#";
+        // on stock l'intance qui va permetre de faire les match dans routes
+        $routes = new Request($paht, $action);
+        // stocage de route dans request
+        self::$request['POST'][] = $routes;
+        return $routes;
+    }
 
-        if (preg_match($pathToMatch,$url, $matches) ) {
-            $this->matches = $matches;
-            return true;
-        } else{
-            return false;
+    public static function run()
+    {
+        // on recupere la methode auto passer en parametree 
+        // on se retrouve avec l'instance de la class request dans route
+        // echo '<pre>';
+        // var_dump(self::$request['GET']);
+        foreach (self::$request[$_SERVER['REQUEST_METHOD']] as $route) 
+        {
+            //on match nos route
+            if ( $route->match(trim($_GET['url'], '/')) ) 
+            {
+                // on execute tous les routes
+                $route->execute();
+                // on quite des qu'on trouve une route correspondante
+                die();
+            }
         }
-        
+        header('HTTP/1.0 404 Not found');
     }
-
-    public function execute()
+    public static function url(string $name, $parametres = [])
     {
-        $params = explode('@', $this->action);
-        $controller = new $params[0]();
-        $method = $params[1];
-        return isset($this->matches[1]) ? $controller->$method($this->matches[1]) :$controller->$method();
+        foreach (self::$request as $key => $value) {
+            foreach (self::$request[$key] as $routes) {
+                if (array_key_exists($name, $routes->name())) 
+                {
+                    $route = $routes->name();
+                    $path = implode("",$route[$name]);
+                    if (!empty($parametres)) 
+                    {
+                        foreach ($parametres as $key => $value) 
+                        {
+                            $url = str_replace("{{$key}}", $value, $path);
+                            return '/'.$url;
+                        }
+                    } else {
+                        return '/'.$path;
+                    }
+                }
+            }
+        }
     }
 }
