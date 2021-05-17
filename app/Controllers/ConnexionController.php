cf1032ccd63ca2706b4c3b541e5a96644828d5ca
Anyaronke Samuel <nous.devellonpons@gmail.com>
2021-05-17 23:52:39 +0200

    Connexion class

---
+++ app/Controllers/ConnexionController.php
@@ -0,0 +1,74 @@
+<?php
+namespace App\Controllers;
+
+use Alert;
+use App\Core\SecuriteHTML;
+use App\Helpers\Helper;
+use App\Https\HttpRequest;
+use App\Models\UserModel;
+
+class ConnexionController extends Controller {
+
+    public function index()
+    { 
+        if (SecuriteHTML::checkedAccess()) {
+            if (Helper::is_admin()) {
+                header('Location:/dashboard');
+                exit;
+            } else {
+                header('Location:/');
+                exit;
+            }
+        
+        }
+        require "../app/Views/template/login.view.php";
+    }
+
+
+
+    public function requestTraitement(HttpRequest $request)
+    {
+        $request->isValideLogin();
+
+        $userModel = new UserModel();
+        $user = $userModel->findBy(["emails" => $request->postName('email')])->fetch();
+        // var_dump($user);
+        // die();
+        if (!$user) {
+            Alert::setAlert("Le mot de pass et ou l'adresse mail est inncorect", 'danger');
+            header('Location:/connexion');
+            exit;
+        }
+        
+        $getUsers = $userModel->hydrate($user);
+
+        if (!password_verify($request->postName('password'), $getUsers->getPasswords())) {
+            Alert::setAlert("Le mot de pass et ou l'adresse mail est inncorect", 'danger');
+            header('Location:/connexion');
+            exit;
+        }
+        
+        Helper::setSession("auth", [
+            "id" => $getUsers->getIdUser(),
+            "role" => $getUsers->getRoles()
+        ]);        
+        SecuriteHTML::generateCookies();
+
+        if (Helper::is_admin()) {
+            header('Location:/dashboard');
+            exit;
+        } else {
+            header('Location:/');
+            exit;
+        }
+    }
+
+
+    public function logout()
+    {
+        unset($_SESSION['auth']);
+        SecuriteHTML::unsetCookies();
+        session_destroy();
+        header('Location:/');
+    }
+}
