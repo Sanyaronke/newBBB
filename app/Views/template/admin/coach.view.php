8d7a74e1f8e4434f4e0bf2362cc341ae80569bb1
Anyaronke Samuel <nous.devellonpons@gmail.com>
2021-05-17 14:41:24 +0200

    admin: coach management

---
+++ app/Views/template/admin/coach.view.php
@@ -0,0 +1,14 @@
+<div class="d-flex align-items-center justify-content-between">
+    <h1 class="mt-4">Les Coachs</h1>
+    <a style="font-size: 13px;" class="btn bg-primary text-white" href=""><i class="fas fa-user-plus"></i> Ajouter un
+        Coach</a>
+</div>
+
+<div class="card mt-5 shadow-sm p-3 mb-5 bg-body rounded">
+    <div class="card-header">
+        La liste de tous les coachs - BBB
+    </div>
+    <div class="card-body ">
+        <?= $newTable ?>
+    </div>
+</div>
