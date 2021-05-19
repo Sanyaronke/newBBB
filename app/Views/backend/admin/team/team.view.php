eee9a60d9c4fa4f4c788f4c86a08b89deab8e36d
Anyaronke Samuel <nous.devellonpons@gmail.com>
2021-05-19 17:33:37 +0200

    admin List of players and updating view player

---
+++ app/Views/backend/admin/team/team.view.php
@@ -0,0 +1,34 @@
+<div class="d-flex align-items-center justify-content-between">
+    <h1 class="mt-4">Les Joueurs <?= Alert::echoAlert()?></h1>
+    <button style="font-size: 13px;" type="button" class="btn btn-primary" data-bs-toggle="modal"
+        data-bs-target="#exampleModal">
+        <i class="fas fa-user-plus"></i>Ajouter un Joueur
+    </button>
+</div>
+
+<div class="card my-4 shadow-sm p-3 mb-5 bg-body rounded">
+    <div class="card-header ">
+        <i class="fas fa-table mr-1"></i>
+        <span class="text-warning "><strong>Mis a jour Urgens</strong></span>
+
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <?= $updateTable ?>
+        </div>
+    </div>
+</div>
+<?php
+echo "<pre>";
+$po = [];
+$data = array();
+$li = 'licences';
+foreach ($players as $key => $value) {
+    
+    //array_push($data[$key], $value->licences);
+    // $data[$key] = [$value->licences];
+    array_push($data, $value->$li);
+}
+
+var_dump($data);
+echo "</pre>";
---
+++ app/Views/backend/admin/team/teamUpdate.view.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Helpers\Helper;
+?>
+
+<div class="d-flex align-items-center justify-content-between">
+    <h1 class="mt-4">Modification de <?= $players->last_names ?></h1>
+    <a style="font-size: 13px;" class="btn bg-primary text-white" href="<?= Helper::routename('admin.team') ?>"><i class="fas fa-user-plus"></i> Liste Des Joueurs</a>
+</div>
+<div class="d-flex justify-content-around py-5 px-3">
+    <div class="col">
+        <div class="row justify-content-center">
+            <div class="col">
+                <div class="card border-0 rounded-lg">
+                    <div class="card-header">
+                        <h3 class="text-center font-weight-light my-4">Modification <?= Alert::echoAlert() ?></h3>
+                    </div>
+                    <div class="card-body">
+                        <?=$createForm?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
